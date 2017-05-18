c33137a74556d99af7aace738f76454a738e82d7
ditero <anelesigenu@gmail.com>
2017-05-18 16:22:06 +0200

    Using Forms

--- index.js
+++ index.js
@@ -1,75 +1,18 @@
-// // grab the packages we need
-// var express = require('express');
-// var app = express();
-// // var greetUsers = require('./myUsers');
-// //
-// // //Instantiate the the Route
-// // var greetUsers = greetUsers();
-//
-// var port = 3000;
-//
-// var listArray = [];
-//
-// //Route Parameter Middleware
-// // parameter middleware that will run before the next routes
-// app.param('name', function(req, res, next, name) {
-//
-//     // check if the user with that name exists
-//     // do some validations
-//     var modified = name;
-//
-//     // save name to the request
-//     req.name = modified;
-//
-//     next();
-// });
-// //var users = req.params.users;
-//
-// // http://localhost:3000/greetings/Joe
-// // app.get('/greetings/', greetUsers.index);
-// // app.get('/greetings/greet', greetUsers.greet);
-// app.get('/greetings/:name', function(req, res) {
-//     // var foundUser = listArray.find(function(currentUser){
-//     //   return currentUser = req.name;
-//     // })
-//     // if (!foundUser) {
-//       listArray.push(req.name);
-//     //}
-//
-//
-//     res.send('Hello ' + req.name + '!');
-// });
-// var list = []
-// // http://localhost:3000/greeted
-// app.get('/greeted/', function(req, res){
-//   listArray.forEach(function(name){
-//     list.push("<a href= counter/" + name + ">" + name + "</a>");
-//   });
-//   res.send('My list: '+ list);
-//
-// });
-//
-// // http://localhost:3000/counter/Joe
-// app.get('/counter/:name', function(req, res){
-//   var count = 0;
-//   for (var i = 0; i < listArray.length; i++) {
-//     if (req.name === listArray[i]) {
-//       count++;
-//     }
-//   }
-//   res.send('Hello, '+req.name + ' is greeted for the ' + count +' times');
-// });
-//
-// ////////////////////////////////Start THe Server//////////////
-//
-// app.listen(port);
-// console.log('Server started! At http://localhost:' + port);
-
-
-
  const express = require('express');
+ const exphbs = require('express3-handlebars');
+ const bodyParser = require('body-parser');
  var greetUsers = require('./myusers');
  const app = express();
+
+ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+ app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
+// prase application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
 
 //Instantiate the routes
 var greetUsers = greetUsers();
@@ -77,10 +20,11 @@ var greetUsers = greetUsers();
  app.get('/', function(req, res){
    res.send('LetsGreet');
  });
- app.get('/greetings', greetUsers.index);
- app.get('/greetings/:users', greetUsers.greet);
+// app.get('/greetings', greetUsers.index);
+ app.get('/greetings', greetUsers.greetScreen);
  app.get('/greeted', greetUsers.greeted);
- app.get('/counter/:users', greetUsers.countGreetings);
+ app.get('/counter/:user', greetUsers.countGreetings);
+ app.post('/greetings', greetUsers.greet);
 
 
 
--- myusers.js
+++ myusers.js
@@ -1,32 +1,42 @@
+'use strict'
 module.exports = function(){
 
-  const index = function(req, res){
-    res.send('Greet Users');
-  };
 ///////////GETUSER FUNCTION: RETUNRS THE VARIABLE <USER>/////////
   function getUser(req, res){
 
-    var user = req.params.users;
+  //  var user = req.params.user;
+  var user = req.body.user;
     return user;
   }
+
 var greetedUsers = [];
 var uniqueList = [];
 
+const index = function(req, res){
+  res.render('myusers/index');
+};
+
+const greetScreen = function(req, res){
+  res.render("myusers/greet");
+
+};
 ////////////////GREETED FUNCTION: RESPONDS WITH THE LIST OF GREETED USERS THAT HAVE LINKS/////
 const greeted = function(req, res){
   //greetedUsers.forEach(function());
   var list = [];
   var user = getUser(req, res);
-  uniqueList.forEach(function(user){
-    list.push("<a href= counter/" + user + ">" + user + "</a>");
-  });
-  res.send('My list: '+list);
+
+  // uniqueList.forEach(function(user){
+  //   list.push("<a href= counter/" + user + ">" + user + "</a>");
+  // });
+
+  res.render('myusers/index',{myusers: uniqueList});
 
 }
 //////GREET FUNCTION: RESPONDS AND PUSHES NAMES TO AN ARRAY///////
   const greet = function(req, res){
   //  res.send('Greet a user');
-  var user = getUser(req, res);
+  var user = req.body.user;
   var foundUser = uniqueList.find(function(currentUser){
     return currentUser === user;
   });
@@ -35,13 +45,13 @@ const greeted = function(req, res){
   }
   greetedUsers.push(user);
     //res.redirect('/greeted');
-    res.send('Hello, '+ user +'!');
+    res.render('/greetings', {});
   }
 
 /////////////COUNTGREETINGS FUNCTION: COUNTS HOW TIMES A USER HAS BEEN GREETED///////
   const countGreetings = function(req, res){
     var count = 0;
-    var user = getUser(req, res);
+    var user = req.params.user;
       for (var i = 0; i < greetedUsers.length; i++) {
         if (user === greetedUsers[i]) {
           count++;
@@ -55,6 +65,7 @@ const greeted = function(req, res){
     greet,
     index,
     greeted,
-    countGreetings
+    countGreetings,
+    greetScreen
   }
 }
