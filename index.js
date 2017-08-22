86d8dee75be99fc2513874627c827681b512ed56
ditero <anelesigenu@gmail.com>
2017-08-22 10:34:05 +0200

    First commit updates

--- index.js
+++ index.js
@@ -3,23 +3,9 @@
  const bodyParser = require('body-parser');
  const flash = require('express-flash');
  const session = require('express-session');
- var greetUsers = require('./myusers');
- var MongoClient = require('mongodb').MongoClient, format = require('util').format;
+ var registrations = require('./functions/registrations');
 
  const app = express();
-
-// Connect to Mongoose
-MongoClient.connect('mongodb://127.0.0.1:27017/test', function(err, db){
-  if (err) {
-    throw err;
-  }else {
-    console.log("successfully connected to the database");
-  }
-  db.close();
-
-});
-
-
 
  app.engine('handlebars', exphbs({defaultLayout: 'main'}));
  app.set('view engine', 'handlebars');
@@ -36,20 +22,14 @@ app.use(bodyParser.json());
 app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 * 30}}));
 app.use(flash());
 //Instantiate the routes
-var greetUsers = greetUsers();
+var register = registrations();
 
- app.get('/', function(req, res){
-   res.send('LetsGreet');
- });
-// app.get('/greetings', greetUsers.index);
- app.get('/greetings', greetUsers.greetScreen);
- app.get('/greeted', greetUsers.greeted);
- app.get('/counter/:user', greetUsers.countGreetings);
- app.post('/greetings', greetUsers.greet);
+app.get('/reg_number', register.main);
+app.post('/reg_number', register.bodyReg);
+app.get('/reg_number/:number', register.paramReg);
 
 
-
- const port = 3000;
+ const port = 3001;
  app.listen(port, function(){
    console.log("Web app started on port: "+port);
  });
