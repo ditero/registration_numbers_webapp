9a9ff27ef3a6a394e33eb516405fa73265ed9f36
ditero <anelesigenu@gmail.com>
2017-08-29 10:39:48 +0200

    Adding functionality on btn Show All

--- functions/registrations.js
+++ functions/registrations.js
@@ -2,71 +2,87 @@
 module.exports = function(models) {
 
   ////////////////////////////FIND REGISTRATION NUMBERS FROM MONGOOSE DATABASE//////////////////////////////////
-const findCollection = function(cb) {
-  models.registrations.find({})
-  .exec(function(err, results) {
-    if (err) {
-      return cb(err);
-    }
-    cb(null, results);
-  });
-};
+  const findCollection = function(cb) {
+    models.registrations.find({})
+      .exec(function(err, results) {
+        if (err) {
+          return cb(err);
+        }
+        cb(null, results);
+      });
+  };
 
-  const regnums = function(req, res){
-    findCollection(function(err, results){
+  const regnums = function(req, res) {
+    findCollection(function(err, results) {
       res.render('regview/regnumbers', {
-        registrationNum : results
+        registrationNum: results
       })
     })
   }
 
-  const paramReg = function(req, res){
+  const paramReg = function(req, res) {
     var registrationNum = req.params.number
     res.render('regview/regnumbers', {
-      registrationNum : registrationNum
+      registrationNum: registrationNum
     })
 
   }
 
-  const addReg =function(req, res, next){
+  const addReg = function(req, res, next) {
     var registration = {
-    regnum: req.body.regplate
+      regnum: req.body.regplate
     }
-    // listReg.push(registration.regnum);
-    models.registrations.create(registration, function(err, results){
-      if (err) {
-        if (err.code === 11000) {
-          req.flash('error', 'This number already exits');
+
+    if (!registration.regnum) {
+      req.flash('error', 'Please add a registration number');
+      res.redirect('/reg_number');
+    } else {
+      models.registrations.create(registration, function(err, results) {
+        if (err) {
+          if (err.code === 11000) {
+            req.flash('error', 'This number already exits');
+            res.redirect('/reg_number');
+          }
+          // return next(err);
+        } else {
           res.redirect('/reg_number');
         }
-        // return next(err);
-      }else {
-        res.redirect('/reg_number');
-      }
-    });
-
+      });
+    }
   }
 
 
-const filterTown = function(req, res, next){
-  var regNumber = req.body.town;
-  models.registrations.find({regnum :{$regex: regNumber, $options: "x"}})
-  .exec(function(err, results){
-    if (err) {
-      return next(err);
+  const filterTown = function(req, res, next) {
+    var regNumber = req.body.town;
+
+    if (regNumber === undefined) {
+      req.flash('error', 'Please choose town to display');
+      res.redirect('/reg_number');
     }else {
-      res.render('regview/regnumbers',{
-        registrationNum : results
+      models.registrations.find({
+        regnum: {
+          $regex: regNumber,
+          $options: "x"
+        }
       })
+      .exec(function(err, results) {
+        if (err) {
+          return next(err);
+        } else {
+          res.render('regview/regnumbers', {
+            registrationNum: results
+          })
+        }
+      });
+
     }
-  });
 
-};
+  };
 
-return{
-  regnums,
-  paramReg,
-  addReg,
-  filterTown
-}
+  return {
+    regnums,
+    paramReg,
+    addReg,
+    filterTown
+  }
 }
